867389fa4f50db8092537c2800990a449412a774
Vishal <vishivish18@gmail.com>
2015-05-13 08:06:06 +0530

    textarea

---
+++ js/draw.js
@@ -0,0 +1,24 @@
+function drawShape(){
+  // get the canvas element using the DOM
+  var canvas = document.getElementById('mycanvas');
+ 
+  // Make sure we don't execute when canvas isn't supported
+  if (canvas.getContext){
+ 
+    // use getContext to use the canvas for drawing
+    var ctx = canvas.getContext('2d');
+ 
+    ctx.lineWidth =1 ;
+    ctx.beginPath();  
+    ctx.moveTo(1366/2,768/2);
+    ctx.lineTo(5,100);
+    ctx.lineTo(300,100);
+    ctx.lineTo(5,5);
+      
+    ctx.stroke();
+      
+
+  } else {
+    alert('You need Safari or Firefox 1.5+ to see this demo.');
+  }
+}
