528b21abd2e5cb72b9e17888ba87675223e40ca4
Vishal <vishivish18@gmail.com>
2015-05-14 08:35:36 +0530

    LT condition check

--- js/grammar.js
+++ js/grammar.js
@@ -7,6 +7,13 @@ console.log('in here');
     
 
 }
+else if(document.getElementById("commandBox").value.toUpperCase() == "LT 90")
+{
+console.log('in here');
+    
+
+}    
+    
     else{
     console.log("error");
     }
