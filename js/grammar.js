b8aac35d512d8f489b4e4d10152886f4432ce5a4
Vishal <vishivish18@gmail.com>
2015-05-15 19:18:16 +0530

    definition to be decided

--- js/grammar.js
+++ js/grammar.js
@@ -11,11 +11,12 @@ else if(document.getElementById("commandBox").value.toUpperCase() == "LT 90")
 {
 console.log('in here');
     
-
 }    
     
     else{
     console.log("error");
+        //////language definition
+        
     }
 
 }
