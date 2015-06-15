08b60f5603c68a8c3f5f5648f212f595cacf6c0a
Vishal <vishivish18@gmail.com>
2015-06-16 02:47:07 +0530

    turtle

--- js/grammar.js
+++ js/grammar.js
@@ -4,7 +4,8 @@ function checkCommand(){
 if(document.getElementById("commandBox").value.toUpperCase() == "RT 90")
 {
 console.log('in here');
-    
+$('#commandBox').val("in here"); 
+
 
 }
 else if(document.getElementById("commandBox").value.toUpperCase() == "LT 90")
